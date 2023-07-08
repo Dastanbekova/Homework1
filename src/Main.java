c1edfc04b74c9038a70ca7a6490a82a246a93a3f
Zuura <zuura@MacBook-Air-Zuura.local>
2023-07-08 20:11:15 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UcBurchtuk burchtuk = new UcBurchtuk();
+        burchtuk.a = 5.4;
+        burchtuk.b = 4.4;
+        burchtuk.c = 8.9;
+
+        System.out.println(burchtuk.arrea(burchtuk.a, burchtuk.b, burchtuk.c));
+
+
     }
 }
--- src/UcBurchtuk.java
+++ src/UcBurchtuk.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class UcBurchtuk {
-}
+public class UcBurchtuk {
+    double a;
+    double b;
+    double c;
+
+      public  double  arrea(double a,double b,double c) {
+          double v = (a + b + c) / 2;
+          double n =v* (v - a) * (v - b )* (v - c);
+
+          return  Math.sqrt(n);
+}}
